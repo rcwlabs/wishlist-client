aff668bfdac70c655eed472c4c273fe9e5cfc8d7
theperfectfuel <robert@theperfectfuel.com>
2017-12-22 20:08:11 -0800

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+
+        };
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>Hey it's the app</h1>
+            </div>
+        );
+    };
+}
