32c281b4e343ba29fc5c96b89bf1782d77998dd6
theperfectfuel <robert@theperfectfuel.com>
2017-12-28 01:21:24 -0800

    adds gift list item component

--- src/components/GiftList.js
+++ src/components/GiftList.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
 
+import GiftListItem from './GiftListItem';
+
 export default class GiftList extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            giftList: [
+                {   key: 'slkdfjlsdk',
+                    recipient: 'Janel',
+                    giftName: 'Car',
+                    price: 23000
+                },
+                {   key: 'slkdlkjllk',
+                    recipient: 'Janel',
+                    giftName: 'House',
+                    price: 323000
+                },
+                {   key: 'sljjklksdk',
+                    recipient: 'Janel',
+                    giftName: 'Purse',
+                    price: 200
+                }
+            ]
+        };
+
+        this.buildGiftList = this.buildGiftList.bind(this);
+    }
+
+    buildGiftList() {
+        return this.state.giftList.map(gift => {
+            return (
+                <GiftListItem
+                    key={gift.key}
+                    recipient={gift.recipient}
+                    giftName={gift.giftName}
+                    price={gift.price}
+                />
+            )
+        });
     }
 
     render() {
@@ -13,9 +48,7 @@ export default class GiftList extends Component {
                 <div className="card-block">
                     <h4 className="card-header">Gift List</h4>
                     <ul>
-                        <li>Item 1</li>
-                        <li>Item 2</li>
-                        <li>Item 3</li>
+                        {this.buildGiftList()}
                     </ul>
                 </div>
             </div>
